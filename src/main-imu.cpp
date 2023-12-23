ed604d3bf4aeedd5d0bc5aff7ede0dcba4512237
Joseph Duchesne <josephgeek@gmail.com>
2023-12-23 00:49:26 -0500

    Initial test scripts for bdb16 bringup

---
+++ src/main-imu.cpp
@@ -0,0 +1,54 @@
+/*
+  Arduino LSM6DS3 - Simple Accelerometer
+
+  This example reads the acceleration values from the LSM6DS3
+  sensor and continuously prints them to the Serial Monitor
+  or Serial Plotter.
+
+  The circuit:
+  - Arduino Uno WiFi Rev 2 or Arduino Nano 33 IoT
+
+  created 10 Jul 2019
+  by Riccardo Rizzo
+
+  This example code is in the public domain.
+*/
+#include <Wire.h>
+#include <Arduino_LSM6DS3.h>
+
+//LSM6DS3Class IMU_LSM6DS3(Wire, LSM6DS3_ADDRESS);
+
+void setup() {
+  Serial1.begin(115200);
+
+  if (!IMU.begin()) {
+    Serial1.println("Failed to initialize IMU!");
+
+    while (1);
+  }
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial1.print("Accelerometer sample rate = ");
+  Serial1.print(IMU.accelerationSampleRate());
+  Serial1.println(" Hz");
+  Serial1.println();
+  Serial1.println("Acceleration in g's");
+  Serial1.println("X\tY\tZ");
+}
+
+void loop() {
+  float x, y, z;
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+    Serial1.print(x);
+    Serial1.print('\t');
+    Serial1.print(y);
+    Serial1.print('\t');
+    Serial1.println(z);
+
+    analogWrite(LED_BUILTIN, (uint16_t)(z*1024));
+  }
+}
---
+++ variants/bdb16/pins_arduino.h
@@ -0,0 +1,60 @@
+#pragma once
+
+// Pin definitions taken from:
+//    https://datasheets.raspberrypi.org/pico/pico-datasheet.pdf
+
+
+// LEDs
+#define PIN_LED        (27u)
+
+// IO pins
+static const uint8_t IO0 = (0u);
+static const uint8_t IO1 = (1u);
+static const uint8_t IO2 = (2u);
+static const uint8_t IO3 = (3u);
+static const uint8_t IO4 = (4u);
+static const uint8_t IO5 = (5u);
+
+// DShot pins
+static const uint8_t DS0 = (6u);
+static const uint8_t DS1 = (7u);
+static const uint8_t DS2 = (8u);
+static const uint8_t DS3 = (9u);
+
+// Board function specific pins
+#define PIN_RGB (19u)
+#define IMU_INT1 (23u)
+#define PIN_SPI0_CS0 (21u)
+#define PIN_SPI0_CS1 (20u)
+#define PIN_SPI0_CS2 (18u)
+
+// Serial
+#define PIN_SERIAL1_TX (0u)
+#define PIN_SERIAL1_RX (1u)
+
+#define PIN_SERIAL2_TX (16u)
+#define PIN_SERIAL2_RX (17u)
+
+// SPI
+#define PIN_SPI0_MISO  (12u)
+#define PIN_SPI0_MOSI  (11u)
+#define PIN_SPI0_SCK   (10u)
+#define PIN_SPI0_SS    PIN_SPI0_CS0
+
+// #define PIN_SPI1_MISO  (12u)
+// #define PIN_SPI1_MOSI  (15u)
+// #define PIN_SPI1_SCK   (14u)
+// #define PIN_SPI1_SS    (13u)
+
+// Wire
+#define PIN_WIRE0_SDA  (24u)
+#define PIN_WIRE0_SCL  (25u)
+
+#define PIN_WIRE1_SDA  (24u)
+#define PIN_WIRE1_SCL  (25u)
+
+#define SERIAL_HOWMANY (2u)
+#define SPI_HOWMANY    (1u)
+#define WIRE_HOWMANY   (1u)
+
+#include "common.h"
